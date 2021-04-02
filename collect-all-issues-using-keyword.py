34f03a1adbe19aee441a7b25061bac1d3fa49422
Reza Jebeli <rezajebeli97@gmail.com>
2021-04-02 10:57:12 -0400

    Update collect-all-issues-using-keyword.py

--- collect-all-issues-using-keyword.py
+++ collect-all-issues-using-keyword.py
@@ -11,10 +11,10 @@ def webscrape(issueState, issueList, keywords, status, topic):
     print('Number of {} issues to be explored : {}'.format(issueState, len(issueList)))
 
     for keyword in keywords:
-    	if status == 'test':
-    		files.append(open('issues/test/{}/{}/All-{}-Issues-Having-{}-in-{}.txt'.format(topic, issueState, issueState, keyword, framework), 'w+', encoding='utf-8'))
-    	else:
-    		files.append(open('issues/{}/{}/All-{}-Issues-Having-{}-in-{}.txt'.format(topic, issueState, issueState, keyword.replace('\\','').replace('/','-'), framework), 'w+', encoding='utf-8'))
+        if status == 'test':
+            files.append(open('issues/test/{}/{}/All-{}-Issues-Having-{}-in-{}.txt'.format(topic, issueState, issueState, keyword, framework), 'w+', encoding='utf-8'))
+        else:
+            files.append(open('issues/{}/{}/All-{}-Issues-Having-{}-in-{}.txt'.format(topic, issueState, issueState, keyword.replace('\\','').replace('/','-'), framework), 'w+', encoding='utf-8'))
 
     for issue in issueList:
         with open(issue, 'r', encoding='utf-8') as f:
@@ -22,23 +22,24 @@ def webscrape(issueState, issueList, keywords, status, topic):
             soup = BeautifulSoup(f.read(), 'html5lib')
 
             #getting the first post which will correspond to the issue's description
-	        # issueDesc = soup.find('td', class_="d-block comment-body markdown-body js-comment-body")
+            # issueDesc = soup.find('td', class_="d-block comment-body markdown-body js-comment-body")
 
-	        #extracting the issue's ID from the bug report
+            #extracting the issue's ID from the bug report
             issueIdMatch = re.search('\\d+', issue)
             issueId = issue[issueIdMatch.start() : issueIdMatch.end()]
 
             for i, keyword in enumerate(keywords):
-	            #check if the html has the keyword
-	            stringsContainingTheKeyword = soup.body.findAll(text=re.compile(keyword))
-	            NumOfRepeat = len(stringsContainingTheKeyword)
-	            if (NumOfRepeat > 0):
-	                issueInfo = {
-	                'ID' : issueId,
-	                # 'Description' : issueDesc,
-	                'Num of Repeat': NumOfRepeat
-	                }
-	                files[i].write('ID: {}\tNum of Repeat: {}\n'.format(issueInfo['ID'], issueInfo['Num of Repeat']))
+                #check if the html has the keyword
+                issueDesc = soup.find('td', class_="d-block comment-body markdown-body js-comment-body")
+                stringsContainingTheKeyword = issueDesc.findAll(text=re.compile(keyword))
+                NumOfRepeat = len(stringsContainingTheKeyword)
+                if (NumOfRepeat > 0):
+                    issueInfo = {
+                    'ID' : issueId,
+                    # 'Description' : issueDesc,
+                    'Num of Repeat': NumOfRepeat
+                    }
+                    files[i].write('ID: {}\tNum of Repeat: {}\n'.format(issueInfo['ID'], issueInfo['Num of Repeat']))
 
 
 def writeResults(issueState, issuesInfo, keyword):
@@ -77,35 +78,36 @@ keywords = []
 
 
 if issueState == 'open' and status == 'real':
-	# issues = ['../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/open/{}'.format(framework, f) for f in os.listdir('../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/open'.format(framework))]
+    # issues = ['../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/open/{}'.format(framework, f) for f in os.listdir('../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/open'.format(framework))]
     issues = ['{}/open/{}'.format(framework, f) for f in os.listdir('{}/open'.format(framework))]
 
 elif issueState == 'closed' and status == 'real':
-	# issues = ['../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/closed/{}'.format(framework, f) for f in os.listdir('../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/closed'.format(framework))]
+    # issues = ['../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/closed/{}'.format(framework, f) for f in os.listdir('../polygot_in_dl_frameworks/polygot_in_dl_frameworks/{}/closed'.format(framework))]
     issues = ['{}/closed/{}'.format(framework, f) for f in os.listdir('{}/closed'.format(framework))]
 
 elif issueState == 'open' and status == 'test':
-	issues = ['../Test/{}/open/{}'.format(framework, f) for f in os.listdir('../Test/{}/open'.format(framework))]
+    issues = ['../Test/{}/open/{}'.format(framework, f) for f in os.listdir('../Test/{}/open'.format(framework))]
 
 elif issueState == 'closed' and stauts == 'test':
-	issues = ['../Test/{}/closed/{}'.format(framework, f) for f in os.listdir('../Test/{}/closed'.format(framework))]
+    issues = ['../Test/{}/closed/{}'.format(framework, f) for f in os.listdir('../Test/{}/closed'.format(framework))]
 
 else:
-	raise Exception("Please enter valid inputs in the command line")
+    raise Exception("Please enter valid inputs in the command line")
 
 security_keywords = ['exception', 'crash', 'security', 'token', 'secret', 'TODO', 'password', 'vulnerable', 'hash', 'HMAC', 'MD5', 'SHA-1', 'SHA-2']#attacker, 
 # performance_keywords = ['performance', 'efficiency', 'efficient', 'fast', 'speed', 'slow', 'memory usage', 'improve', 'memory leak', 'optimize']
 performance_regression_keywords = ['commit', '(1|2)\\.\\d+(\\.\\d+)*', 'tensorflow\\/tensorflow\\/commit\\/\\w+', 'tensorflow\\/tensorflow\\/tree\\/', 'tensorflow\\/tensorflow\\/releases\\/', 'release']
+
 if topic == "security":
-	webscrape(issueState, issues, security_keywords, status, topic)
+    webscrape(issueState, issues, security_keywords, status, topic)
 elif topic == "performance_regression":
-	webscrape(issueState, issues, performance_regression_keywords, status, topic)
+    webscrape(issueState, issues, performance_regression_keywords, status, topic)
 elif topic == "both":
-	webscrape(issueState, issues, security_keywords, status, 'security')
-	topic = 'performance_regression'
-	webscrape(issueState, issues, performance_regression, status, 'performance_regression')
+    webscrape(issueState, issues, security_keywords, status, 'security')
+    topic = 'performance_regression'
+    webscrape(issueState, issues, performance_regression, status, 'performance_regression')
 else:
-	raise Exception("Wrong topic")
+    raise Exception("Wrong topic")
 
 timeTaken = time.time() - start
 
